675c015a2552c6301b96dedfd324d0fdf6c2a354
Velvel <28vrichmondshazeer@gannacademy.org>
2024-10-01 09:21:29 -0400

    Commited

--- src/App.java
+++ src/App.java
@@ -2,6 +2,10 @@ import processing.core.*;
 
 public class App extends PApplet {
     float mouthSize;
+    float twelthRoot = (float) Math.pow(2, 1.0 / 12.0);
+    float minDiameter = 6.25f;
+    float maxDiameter = 100;
+
     public static void main(String[] args) {
         PApplet.main("App");
     }
@@ -12,7 +16,7 @@ public class App extends PApplet {
 
     public void setup() {
         background(200);
-        mouthSize = 100;
+        mouthSize = 25;
     }
 
     public void draw() {
@@ -22,18 +26,27 @@ public class App extends PApplet {
         ellipse(300, 300, 50, 60);
         ellipse(400, 300, 50, 60);
         fill(255, 100, 0);
-        triangle(350, 300, 300, 375, 400, 375);
-        fill(0, 255, 255);
+        triangle(350, 300, 300, 350, 400, 350);
+        fill(0, 0, 139);
         ellipse(350, 425, mouthSize, mouthSize);
-    
+        fill(0, 255, 255, 100);
+        rect(312.5f, 350, 75, 75);
+
     }
 
-    public void keyPressed(){
-        if(keyCode == RIGHT){
-            mouthSize += 5;
-        } else {
-            mouthSize -= 5;
+    public void keyPressed() {
+        if (keyCode == RIGHT) {
+            System.out.println("diameter is " + mouthSize);
+            System.out.println("max diameter is " + maxDiameter);
+            if (mouthSize < maxDiameter) {
+                mouthSize *= twelthRoot;
+            }
         }
-        
+        if (keyCode == LEFT) {
+            if (mouthSize > minDiameter) {
+                mouthSize /= twelthRoot;
+            }
+        }
+
     }
 }
