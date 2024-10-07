f9bf357099dffc1e9f0966ee84e9ec3d3f0ec397
Velvel <28vrichmondshazeer@gannacademy.org>
2024-10-07 12:52:01 -0400

    Commint

--- src/App.java
+++ src/App.java
@@ -22,17 +22,25 @@ public class App extends PApplet {
     public void draw() {
         fill(255, 255, 0);
         ellipse(350, 350, 300, 300);
-        fill(0);
+        fill(255);
         ellipse(300, 300, 50, 60);
         ellipse(400, 300, 50, 60);
+        fill(0);
+        ellipse(300, 300, 25, 25);
+        ellipse(400, 300, 25, 25);
         fill(255, 100, 0);
         triangle(350, 300, 300, 350, 400, 350);
         fill(0, 0, 139);
-        ellipse(350, 425, mouthSize, mouthSize);
-        fill(0, 255, 255, 100);
+        ellipse(350, 400, mouthSize, mouthSize);
+        fill(0, 255, 255, 150);
         rect(312.5f, 350, 75, 75);
+        fill(255, 255, 0, 100);
+        arc(350, 387.5f, 50, 50, 0, PI);
+        fill(128, 0, 128);
+        textSize(50);
+        text("Play A", 50, 50);
+        }
 
-    }
 
     public void keyPressed() {
         if (keyCode == RIGHT) {
@@ -47,6 +55,9 @@ public class App extends PApplet {
                 mouthSize /= twelthRoot;
             }
         }
-
+        if((int)mouthSize == 100 || (int)mouthSize == 50 || (int)mouthSize == 25 || (int)mouthSize == 12.5 || (int)mouthSize == 6.25) {
+            System.out.println("Played A");
+            text("Great job!", 50, 600);
     }
+}
 }
